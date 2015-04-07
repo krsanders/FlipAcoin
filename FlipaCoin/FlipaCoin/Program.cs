b053641c527e56076022cbff3ff16c181fdc2886
Kris Sanders <sandersksw@gmail.com>
2015-04-07 16:31:24 -0600

    Committing changes (for first time)

--- FlipaCoin/FlipaCoin/Program.cs
+++ FlipaCoin/FlipaCoin/Program.cs
@@ -8,8 +8,36 @@ namespace FlipaCoin
 {
     class Program
     {
+        static Random rng = new Random();
+        static int headsCounter = 0;
+        static int tailsCounter = 0;
+
         static void Main(string[] args)
         {
+            
+            for (int i = 0; i< 1000; i++)
+            {
+                Console.WriteLine("Flipped: " + FlipaCoin());
+            }
+            Console.WriteLine("Heads: " + headsCounter);
+            Console.WriteLine("Tails: " + tailsCounter);
+            Console.ReadKey();
+        }
+        public static string FlipaCoin()
+        {
+            if(rng.Next(2) == 0)
+            {
+                //Heads
+                headsCounter = headsCounter + 1;
+                return "Heads";
+            }
+            else 
+            {
+                //Tails
+                tailsCounter = tailsCounter + 1;
+                return "Tails";
+            }
+
         }
     }
 }
